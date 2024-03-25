4920d6d1b2d8ad8a9e482247c7101edccb69f221
Dilruba YILMAZ <dilrubayilmaz@Dilruba-MacBook-Pro.local>
2024-03-25 16:28:24 +0300

    design is completed

---
+++ sayitahmin/ViewController.swift
@@ -0,0 +1,44 @@
+//  Created by Dilruba YILMAZ on 24.03.2024.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var kullanicisayi: UITextField!
+    @IBOutlet weak var kullanicimg: UIImageView!
+    @IBOutlet weak var kullanicikaydet: UIButton!
+    
+    @IBOutlet weak var tahminsayi: UITextField!
+    @IBOutlet weak var tahminimg: UIImageView!
+    @IBOutlet weak var tahminkaydet: UIButton!
+    
+    @IBOutlet weak var yildiz1: UIImageView!
+    @IBOutlet weak var yildiz2: UIImageView!
+    @IBOutlet weak var yildiz3: UIImageView!
+    @IBOutlet weak var yildiz4: UIImageView!
+    @IBOutlet weak var yildiz5: UIImageView!
+    
+    @IBOutlet weak var lblsonuc: UILabel!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+
+    @IBAction func btnkaydet(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btndene(_ sender: UIButton) {
+    }
+    
+    
+}
+
